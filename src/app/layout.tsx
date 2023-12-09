45c83193126a261df9071afca034e0a745e66c5c
Muhammad Tarreq Maulana <mrtrq>
2023-12-10 00:55:20 +0700

    style: adjusting metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-import type { Metadata } from 'next'
+import { Metadata } from 'next'
 import { Inter, Plus_Jakarta_Sans } from 'next/font/google'
 import './globals.css'
 
@@ -8,6 +8,7 @@ const jakarta = Plus_Jakarta_Sans({subsets: ['latin']})
 export const metadata: Metadata = {
   title: 'Tarreq Maulana',
   description: 'Under construction..',
+  icons: 'https://drive.google.com/file/d/1IaZLNH_CnhLvfEAD4-sFiXzEx4nULQWq/view?usp=sharing'
 }
 
 export default function RootLayout({
