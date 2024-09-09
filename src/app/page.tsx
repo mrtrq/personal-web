d737f318fdc3b041b7aa33a7ec99e0ed235a071e
mrtrq <tarreq.maulana@gmail.com>
2024-09-09 17:00:46 +0700

    responsive main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,9 +4,9 @@ export default function Home() {
   return (
     
     <main className='min-height-screen bg-white p-2 flex justify-center items-center'>
-      <div className='flex flex-col items-center gap-4'>
-        <h1 className='text-[3.5rem]'>Tarreq Maulana</h1>
-        <div className='flex flex-col items-center gap-1'>
+      <div className='flex flex-col items-center justify-center gap-4'>
+        <h1 className='text-[3rem] text-center'>Tarreq Maulana</h1>
+        <div className='flex flex-col items-center justify-center text-center gap-1'>
         <p className='font-light text-l'>A generalist final-year Computer Science student at Universitas Indonesia.</p>
         <p className='font-semibold text-l'>Passionate about leveraging technology to solve real-world problems 💻⚙️</p>
         </div>
