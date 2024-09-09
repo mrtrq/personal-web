3ee2dab4727b7ecba36e1a3f74da5ebb78bb5e72
mrtrq <tarreq.maulana@gmail.com>
2024-09-09 16:54:47 +0700

    modify footer

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,27 +1,34 @@
-// const Footer = () => {
-//     return(
-//         <div className="grid grid-cols-2 absolute bottom-0 left-0 w-full p-4 text-black text-center font-light">
-//             <p>© 2024 Tarreq Maulana. All rights reserved</p>
-//         </div>
-//     )
-// }
-// export default Footer
-
 const Footer = () => {
     return(
-        <div className="grid grid-cols-2 absolute bottom-6 left-0 w-full p-4 text-black text-center font-light">
-            <p>© 2024 Tarreq Maulana. All rights reserved</p>
+        <div className="grid grid-cols-1 absolute bottom-4 left-0 w-full p-4 text-black text-center font-light">
             <div className="flex justify-center space-x-4">
-                <a href="https://twitter.com/plandemic_" target="_blank" rel="noopener noreferrer" className="hover:text-blue-300 transition-colors duration-200 ease-in-out">
-                    Twitter
-                </a>
-                <a href="https://www.linkedin.com/in/tarreqmaulana/" target="_blank" rel="noopener noreferrer" className="hover:text-blue-300 transition-colors duration-200 ease-in-out">
-                    LinkedIn
-                </a>
-                <a href="https://github.com/mrtrq" target="_blank" rel="noopener noreferrer" className="hover:text-blue-300 transition-colors duration-200 ease-in-out">
-                    GitHub
-                </a>
-            </div>
+                  <a href="https://instagram.com/tarreqmaulana" target="_blank" rel="noopener noreferrer" className="hover:text-blue-300 transition-colors duration-200 ease-in-out">
+//                 <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="24" height="24" viewBox="0 0 50 50">
+// <path d="M 16 3 C 8.8324839 3 3 8.8324839 3 16 L 3 34 C 3 41.167516 8.8324839 47 16 47 L 34 47 C 41.167516 47 47 41.167516 47 34 L 47 16 C 47 8.8324839 41.167516 3 34 3 L 16 3 z M 16 5 L 34 5 C 40.086484 5 45 9.9135161 45 16 L 45 34 C 45 40.086484 40.086484 45 34 45 L 16 45 C 9.9135161 45 5 40.086484 5 34 L 5 16 C 5 9.9135161 9.9135161 5 16 5 z M 37 11 A 2 2 0 0 0 35 13 A 2 2 0 0 0 37 15 A 2 2 0 0 0 39 13 A 2 2 0 0 0 37 11 z M 25 14 C 18.936712 14 14 18.936712 14 25 C 14 31.063288 18.936712 36 25 36 C 31.063288 36 36 31.063288 36 25 C 36 18.936712 31.063288 14 25 14 z M 25 16 C 29.982407 16 34 20.017593 34 25 C 34 29.982407 29.982407 34 25 34 C 20.017593 34 16 29.982407 16 25 C 16 20.017593 20.017593 16 25 16 z"></path>
+// </svg>
+//                 </a>
+//                 <a href="https://twitter.com/plandemic_" target="_blank" rel="noopener noreferrer" className="hover:text-blue-300 transition-colors duration-200 ease-in-out">
+//                 <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="24" height="24" viewBox="0 0 30 30">
+//     <path d="M28,6.937c-0.957,0.425-1.985,0.711-3.064,0.84c1.102-0.66,1.947-1.705,2.345-2.951c-1.03,0.611-2.172,1.055-3.388,1.295 c-0.973-1.037-2.359-1.685-3.893-1.685c-2.946,0-5.334,2.389-5.334,5.334c0,0.418,0.048,0.826,0.138,1.215 c-4.433-0.222-8.363-2.346-10.995-5.574C3.351,6.199,3.088,7.115,3.088,8.094c0,1.85,0.941,3.483,2.372,4.439 c-0.874-0.028-1.697-0.268-2.416-0.667c0,0.023,0,0.044,0,0.067c0,2.585,1.838,4.741,4.279,5.23 c-0.447,0.122-0.919,0.187-1.406,0.187c-0.343,0-0.678-0.034-1.003-0.095c0.679,2.119,2.649,3.662,4.983,3.705 c-1.825,1.431-4.125,2.284-6.625,2.284c-0.43,0-0.855-0.025-1.273-0.075c2.361,1.513,5.164,2.396,8.177,2.396 c9.812,0,15.176-8.128,15.176-15.177c0-0.231-0.005-0.461-0.015-0.69C26.38,8.945,27.285,8.006,28,6.937z"></path>
+// </svg>
+//                 </a>
+//                 <a href="https://www.linkedin.com/in/tarreqmaulana/" target="_blank" rel="noopener noreferrer" className="hover:text-blue-300 transition-colors duration-200 ease-in-out">
+//                 <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="24" height="24" viewBox="0 0 30 30">
+//     <path d="M24,4H6C4.895,4,4,4.895,4,6v18c0,1.105,0.895,2,2,2h18c1.105,0,2-0.895,2-2V6C26,4.895,25.105,4,24,4z M10.954,22h-2.95 v-9.492h2.95V22z M9.449,11.151c-0.951,0-1.72-0.771-1.72-1.72c0-0.949,0.77-1.719,1.72-1.719c0.948,0,1.719,0.771,1.719,1.719 C11.168,10.38,10.397,11.151,9.449,11.151z M22.004,22h-2.948v-4.616c0-1.101-0.02-2.517-1.533-2.517 c-1.535,0-1.771,1.199-1.771,2.437V22h-2.948v-9.492h2.83v1.297h0.04c0.394-0.746,1.356-1.533,2.791-1.533 c2.987,0,3.539,1.966,3.539,4.522V22z"></path>
+// </svg>
+//                 </a>
+//                 <a href="https://github.com/mrtrq" target="_blank" rel="noopener noreferrer" className="hover:text-blue-300 transition-colors duration-200 ease-in-out">
+//                     <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="24" height="24" viewBox="0 0 30 30">
+//     <path d="M15,3C8.373,3,3,8.373,3,15c0,5.623,3.872,10.328,9.092,11.63C12.036,26.468,12,26.28,12,26.047v-2.051 c-0.487,0-1.303,0-1.508,0c-0.821,0-1.551-0.353-1.905-1.009c-0.393-0.729-0.461-1.844-1.435-2.526 c-0.289-0.227-0.069-0.486,0.264-0.451c0.615,0.174,1.125,0.596,1.605,1.222c0.478,0.627,0.703,0.769,1.596,0.769 c0.433,0,1.081-0.025,1.691-0.121c0.328-0.833,0.895-1.6,1.588-1.962c-3.996-0.411-5.903-2.399-5.903-5.098 c0-1.162,0.495-2.286,1.336-3.233C9.053,10.647,8.706,8.73,9.435,8c1.798,0,2.885,1.166,3.146,1.481C13.477,9.174,14.461,9,15.495,9 c1.036,0,2.024,0.174,2.922,0.483C18.675,9.17,19.763,8,21.565,8c0.732,0.731,0.381,2.656,0.102,3.594 c0.836,0.945,1.328,2.066,1.328,3.226c0,2.697-1.904,4.684-5.894,5.097C18.199,20.49,19,22.1,19,23.313v2.734 c0,0.104-0.023,0.179-0.035,0.268C23.641,24.676,27,20.236,27,15C27,8.373,21.627,3,15,3z"></path>
+// </svg>
+//                 </a>
+//                 <a href="https://medium.com/@tarreq.maulana" target="_blank" rel="noopener noreferrer" className="hover:text-blue-300 transition-colors duration-200 ease-in-out">
+//                 <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="24" height="24" viewBox="0 0 50 50">
+// <path d="M 5 4 A 1.0001 1.0001 0 0 0 4 5 L 4 45 A 1.0001 1.0001 0 0 0 5 46 L 45 46 A 1.0001 1.0001 0 0 0 46 45 L 46 5 A 1.0001 1.0001 0 0 0 45 4 L 5 4 z M 6 6 L 44 6 L 44 44 L 6 44 L 6 6 z M 11 13 L 11 13.5 L 12.857422 16 C 12.949422 16.109 13 16.246672 13 16.388672 L 13 31.5 C 13 31.721 12.923203 31.935422 12.783203 32.107422 L 10 35.546875 L 10 36 L 18 36 L 18 35.546875 L 15.214844 32.087891 C 15.075844 31.916891 15 31.701422 15 31.482422 L 15 18.375 L 23.304688 36 L 24.847656 36 L 24.853516 35.980469 L 32 17.878906 L 32 32.75 C 32 32.91 31.935312 33.063734 31.820312 33.177734 L 29 35.546875 L 29 36 L 40 36 L 40 35.546875 L 38.179688 33.177734 C 38.064687 33.063734 38 32.91 38 32.75 L 38 16.125 C 38 15.965 38.064687 15.811266 38.179688 15.697266 L 40 13.5 L 40 13 L 31.775391 13 L 25.996094 27.642578 L 19.097656 13 L 11 13 z M 15.621094 15 L 17.828125 15 L 24.988281 30.195312 L 23.982422 32.744141 L 23.980469 32.744141 L 15.621094 15 z M 34 15 L 36.257812 15 C 36.090813 15.348 36 15.735 36 16.125 L 36 32.75 C 36 33.188 36.113266 33.619 36.322266 34 L 33.677734 34 C 33.886734 33.619 34 33.188 34 32.75 L 34 17.878906 L 34 15 z"></path>
+// </svg>
+//                 </a>
+//             </div>
+            <p>© 2024 Tarreq Maulana. All rights reserved</p>
         </div>
     )
 }
